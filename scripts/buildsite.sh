90d6d0a88e8615be819c07a760ea43e4cd423bf9
Camden Jansen <csjansen@uci.edu>
2015-07-10 12:46:31 -0700

    You no longer have to set up Apache Directory Listings to get SOMatic to work.

--- scripts/buildsite.sh
+++ scripts/buildsite.sh
@@ -34,11 +34,12 @@ rm -rf ../Som_Package
 rm -rf ../$SOMName
 tar -C ../ -zxf ../Som_Package.tgz
 mv ../Som_Package ../$SOMName
-sed -i -e "s/= 30/= $Rows/g" ../$SOMName/options.js
+cp $SampleList ../$SOMName/data/sample.list
+sed -i -e "s/= 20/= $Rows/g" ../$SOMName/options.js
 sed -i -e "s/= 50/= $Cols/g" ../$SOMName/options.js
 ../bin/train/trainsom -Rows $Rows -Cols $Cols -TrainingMatrix $Matrix -SOMFile ../$SOMName.som -Trials $Trials -Timesteps $Timesteps -Topology toroid
 ../bin/score/scoresom -SOMFile ../$SOMName.som -TrainingMatrix $Matrix -ScoreFile ../$SOMName.score
-../bin/map/mapsom -SOMFile ../$SOMName.som -SampleList $SampleList -Prefix ../$SOMFile/data/som/map_
+../bin/map/mapsom -SOMFile ../$SOMName.som -SampleList $SampleList -Prefix ../$SOMFile/data/som/
 mv ../$SOMName/data/som/map_summery.map ../$SOMName/data/map_summery.map
 ../bin/units/getunits -ScoreFile ../$SOMName.score -Rows $Rows -Cols $Cols -Prefix ../$SOMName/data/som/units/unit
 
